c64be0ba3e0ed23218faace5c98c606a56a141bc
Nitin Kumar <nk525888@gmail.com>
2019-11-06 22:02:44 +0530

    Added checking for primality

---
+++ Lecture 14/IsPrime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n == 1)
+        return false;
+    for(int i=2; i*i<=n; i++){
+        if(n%i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int n = 23;
+    if(isPrime(n))
+        cout << n << " is prime" << endl;
+    else
+        cout << n << " is not prime" << endl;
+}
