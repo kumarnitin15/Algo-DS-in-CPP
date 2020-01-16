88cc1dd4962e67335ac475684e6dd3a2a7ea98bc
Nitin Kumar <nk525888@gmail.com>
2020-01-16 21:23:13 +0530

    Added AVL tree deletion

---
+++ Lecture 18/AVLDeletion.cpp
@@ -0,0 +1,114 @@
+Node * minValueNode(Node* node)  
+{  
+    Node* current = node;  
+  
+    /* loop down to find the leftmost leaf */
+    while (current->left != NULL)  
+        current = current->left;  
+  
+    return current;  
+}  
+  
+Node* deleteNode(Node* root, int key)  
+{  
+      
+    // STEP 1: PERFORM STANDARD BST DELETE  
+    if (root == NULL)  
+        return root;  
+  
+    // If the key to be deleted is smaller  
+    // than the root's key, then it lies 
+    // in left subtree  
+    if ( key < root->key )  
+        root->left = deleteNode(root->left, key);  
+  
+    // If the key to be deleted is greater  
+    // than the root's key, then it lies  
+    // in right subtree  
+    else if( key > root->key )  
+        root->right = deleteNode(root->right, key);  
+  
+    // if key is same as root's key, then  
+    // This is the node to be deleted  
+    else
+    {  
+        // node with only one child or no child  
+        if( (root->left == NULL) || 
+            (root->right == NULL) )  
+        {  
+            Node *temp = root->left ?  
+                         root->left :  
+                         root->right;  
+  
+            // No child case  
+            if (temp == NULL)  
+            {  
+                temp = root;  
+                root = NULL;  
+            }  
+            else // One child case  
+            *root = *temp; // Copy the contents of  
+                           // the non-empty child  
+            free(temp);  
+        }  
+        else
+        {  
+            // node with two children: Get the inorder  
+            // successor (smallest in the right subtree)  
+            Node* temp = minValueNode(root->right);  
+  
+            // Copy the inorder successor's  
+            // data to this node  
+            root->key = temp->key;  
+  
+            // Delete the inorder successor  
+            root->right = deleteNode(root->right,  
+                                     temp->key);  
+        }  
+    }  
+  
+    // If the tree had only one node 
+    // then return  
+    if (root == NULL)  
+    return root;  
+  
+    // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE  
+    root->height = 1 + max(height(root->left),  
+                           height(root->right));  
+  
+    // STEP 3: GET THE BALANCE FACTOR OF  
+    // THIS NODE (to check whether this  
+    // node became unbalanced)  
+    int balance = getBalance(root);  
+  
+    // If this node becomes unbalanced,  
+    // then there are 4 cases  
+  
+    // Left Left Case  
+    if (balance > 1 &&  
+        getBalance(root->left) >= 0)  
+        return rightRotate(root);  
+  
+    // Left Right Case  
+    if (balance > 1 &&  
+        getBalance(root->left) < 0)  
+    {  
+        root->left = leftRotate(root->left);  
+        return rightRotate(root);  
+    }  
+  
+    // Right Right Case  
+    if (balance < -1 &&  
+        getBalance(root->right) <= 0)  
+        return leftRotate(root);  
+  
+    // Right Left Case  
+    if (balance < -1 &&  
+        getBalance(root->right) > 0)  
+    {  
+        root->right = rightRotate(root->right);  
+        return leftRotate(root);  
+    }  
+  
+    return root;  
+} 
